7d184391570da6485ad0f2450532c42752beca57
mmntmaulana <mmntmaulana@addu.edu.ph>
2019-01-09 21:57:21 +0800

    initial commit

---
+++ polls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('help/',views.help, name='help'),
+    path('pagtarong/',views.pagtarong, name='pagtarong')
+]
